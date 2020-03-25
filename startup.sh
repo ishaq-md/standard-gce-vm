01a73147ae2f92529d5902ed4b61a0f9dde810b3
unknown <ishaqmd@live.com>
2020-03-25 21:51:22 +0530

    first commit

--- startup.sh
+++ startup.sh
@@ -1,5 +1,14 @@
 sudo su 
 apt-get update
+apt-get install git -y 
+apt-get install unzip -y
+wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip 
+unzip terraform_0.12.0_linux_amd64.zip 
+sudo mv terraform /usr/local/bin/ 
+terraform --version 
 gcloud version
 gcloud init
-
+send "1\r"
+send "1\r"
+send "yes\r" 
+send "7\r"
